858f5a5d64572ce07055f9a10d549c67d6ce4894
COSVM-NETWORK <118684491+COSVM-NETWORK@users.noreply.github.com>
2023-09-23 01:06:58 +0600

    Update start-docker.sh
    cosvm

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -6,22 +6,22 @@ MONIKER="mymoniker"
 DATA_DIR=$(mktemp -d -t evmos-datadir.XXXXX)
 
 echo "create and add new keys"
-./fcod keys add $KEY --home $DATA_DIR --no-backup --chain-id $CHAINID --algo "eth_secp256k1" --keyring-backend test
+./cvmd keys add $KEY --home $DATA_DIR --no-backup --chain-id $CHAINID --algo "eth_secp256k1" --keyring-backend test
 echo "init Evmos with moniker=$MONIKER and chain-id=$CHAINID"
-./fcod init $MONIKER --chain-id $CHAINID --home $DATA_DIR
+./cvmd init $MONIKER --chain-id $CHAINID --home $DATA_DIR
 echo "prepare genesis: Allocate genesis accounts"
-./fcod add-genesis-account \
-"$(./fcod keys show $KEY -a --home $DATA_DIR --keyring-backend test)" 1000000000000000000pose,1000000000000000000stake \
+./cvmd add-genesis-account \
+"$(./cvmd keys show $KEY -a --home $DATA_DIR --keyring-backend test)" 1000000000000000000pose,1000000000000000000stake \
 --home $DATA_DIR --keyring-backend test
 echo "prepare genesis: Sign genesis transaction"
-./fcod gentx $KEY 1000000000000000000stake --keyring-backend test --home $DATA_DIR --keyring-backend test --chain-id $CHAINID
+./cvmd gentx $KEY 1000000000000000000stake --keyring-backend test --home $DATA_DIR --keyring-backend test --chain-id $CHAINID
 echo "prepare genesis: Collect genesis tx"
-./fcod collect-gentxs --home $DATA_DIR
+./cvmd collect-gentxs --home $DATA_DIR
 echo "prepare genesis: Run validate-genesis to ensure everything worked and that the genesis file is setup correctly"
-./fcod validate-genesis --home $DATA_DIR
+./cvmd validate-genesis --home $DATA_DIR
 
 echo "starting evmos node $i in background ..."
-./fcod start --pruning=nothing --rpc.unsafe \
+./cvmd start --pruning=nothing --rpc.unsafe \
 --keyring-backend test --home $DATA_DIR \
 >$DATA_DIR/node.log 2>&1 & disown
 
