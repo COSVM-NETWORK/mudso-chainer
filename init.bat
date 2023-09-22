423cb73d6472b9f900f246140d2186fc5c9c04db
COSVM-NETWORK <118684491+COSVM-NETWORK@users.noreply.github.com>
2023-09-23 00:40:28 +0600

    Update init.bat
    cosvm

--- init.bat
+++ init.bat
@@ -17,32 +17,32 @@ set LOGLEVEL="info"
 # to trace evm
 #TRACE="--trace"
 set TRACE=""
-set HOME=%USERPROFILE%\.fcod
+set HOME=%USERPROFILE%\.cvmd
 echo %HOME%
 set ETHCONFIG=%HOME%\config\config.toml
 set GENESIS=%HOME%\config\genesis.json
 set TMPGENESIS=%HOME%\config\tmp_genesis.json
 
 @echo build binary
-go build .\cmd\fcod
+go build .\cmd\cvmd
 
 
 @echo clear home folder
 del /s /q %HOME%
 
-fcod config keyring-backend %KEYRING%
-fcod config chain-id %CHAINID%
+cvmd config keyring-backend %KEYRING%
+cvmd config chain-id %CHAINID%
 
-fcod keys add %KEY% --keyring-backend %KEYRING% --algo %KEYALGO%
+cvmd keys add %KEY% --keyring-backend %KEYRING% --algo %KEYALGO%
 
 rem Set moniker and chain-id for Evmos (Moniker can be anything, chain-id must be an integer)
-fcod init %MONIKER% --chain-id %CHAINID% 
+cvmd init %MONIKER% --chain-id %CHAINID% 
 
-rem Change parameter token denominations to fco
-cat %GENESIS% | jq ".app_state[\"staking\"][\"params\"][\"bond_denom\"]=\"fco\""   >   %TMPGENESIS% && move %TMPGENESIS% %GENESIS%
-cat %GENESIS% | jq ".app_state[\"crisis\"][\"constant_fee\"][\"denom\"]=\"fco\"" > %TMPGENESIS% && move %TMPGENESIS% %GENESIS%
-cat %GENESIS% | jq ".app_state[\"gov\"][\"deposit_params\"][\"min_deposit\"][0][\"denom\"]=\"fco\"" > %TMPGENESIS% && move %TMPGENESIS% %GENESIS%
-cat %GENESIS% | jq ".app_state[\"mint\"][\"params\"][\"mint_denom\"]=\"fco\"" > %TMPGENESIS% && move %TMPGENESIS% %GENESIS%
+rem Change parameter token denominations to cvm
+cat %GENESIS% | jq ".app_state[\"staking\"][\"params\"][\"bond_denom\"]=\"cvm\""   >   %TMPGENESIS% && move %TMPGENESIS% %GENESIS%
+cat %GENESIS% | jq ".app_state[\"crisis\"][\"constant_fee\"][\"denom\"]=\"cvm\"" > %TMPGENESIS% && move %TMPGENESIS% %GENESIS%
+cat %GENESIS% | jq ".app_state[\"gov\"][\"deposit_params\"][\"min_deposit\"][0][\"denom\"]=\"cvm\"" > %TMPGENESIS% && move %TMPGENESIS% %GENESIS%
+cat %GENESIS% | jq ".app_state[\"mint\"][\"params\"][\"mint_denom\"]=\"cvm\"" > %TMPGENESIS% && move %TMPGENESIS% %GENESIS%
 
 rem increase block time (?)
 cat %GENESIS% | jq ".consensus_params[\"block\"][\"time_iota_ms\"]=\"30000\"" > %TMPGENESIS% && move %TMPGENESIS% %GENESIS%
@@ -54,18 +54,18 @@ rem setup
 sed -i "s/create_empty_blocks = true/create_empty_blocks = false/g" %ETHCONFIG%
 
 rem Allocate genesis accounts (cosmos formatted addresses)
-fcod add-genesis-account %KEY% 100000000000000000000000000pose --keyring-backend %KEYRING%
+cvmd add-genesis-account %KEY% 100000000000000000000000000pose --keyring-backend %KEYRING%
 
 rem Sign genesis transaction
-fcod gentx %KEY% 1000000000000000000000pose --keyring-backend %KEYRING% --chain-id %CHAINID%
+cvmd gentx %KEY% 1000000000000000000000pose --keyring-backend %KEYRING% --chain-id %CHAINID%
 
 rem Collect genesis tx
-fcod collect-gentxs
+cvmd collect-gentxs
 
 rem Run this to ensure everything worked and that the genesis file is setup correctly
-fcod validate-genesis
+cvmd validate-genesis
 
 
 
 rem Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-fcod start --pruning=nothing %TRACE% --log_level %LOGLEVEL% --minimum-gas-prices=0.0001pose
+cvmd start --pruning=nothing %TRACE% --log_level %LOGLEVEL% --minimum-gas-prices=0.0001pose
